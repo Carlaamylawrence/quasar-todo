ba0852511a81878e01494347840160b0df95d1ad
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-09-29 12:50:38 +0200

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, tenetur
+      natus est, temporibus inventore impedit nesciunt nobis reprehenderit
+      molestiae magni asperiores eaque consectetur suscipit saepe, non omnis
+      dicta deserunt sint!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi deleniti
+      fugiat omnis eaque molestias, reiciendis, blanditiis voluptatum aliquid
+      perferendis, libero provident qui eveniet ex et praesentium nisi similique
+      aperiam ratione!
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ToDo",
+});
+</script>
